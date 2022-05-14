25f21fd8bcc47e33f892f81034403be87d1e56d1
Mathias Bynens <mathias@qiwi.be>
2022-05-14 11:05:12 +0200

    Add Thyria data

--- run.sh
+++ run.sh
@@ -3,3 +3,4 @@
 date="$(date +'%Y-%m-%d')";
 node --no-warnings get-fraggers.mjs 'Gide Lovers' > "data/vunira/fraggers-${date}.json";
 node --no-warnings get-fraggers.mjs 'We dont care' 'Zero Respect' > "data/antica/fraggers-${date}.json";
+node --no-warnings get-fraggers.mjs 'No Impact' > "data/thyria/fraggers-${date}.json";
