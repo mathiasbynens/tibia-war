008387ce65fe6ee9e3374d1cb011f4192f1926e7
Mathias Bynens <mathias@qiwi.be>
2022-05-25 13:37:32 +0200

    Remove dead guild

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 date="$(date +'%Y-%m-%d')";
-node --no-warnings get-fraggers.mjs 'Gide Lovers' 'We dont mind' > "data/vunira/fraggers-${date}.json";
+node --no-warnings get-fraggers.mjs 'We dont mind' > "data/vunira/fraggers-${date}.json";
 node --no-warnings get-fraggers.mjs 'We dont care' 'Zero Respect' > "data/antica/fraggers-${date}.json";
 node --no-warnings get-fraggers.mjs 'No Impact' > "data/thyria/fraggers-${date}.json";
