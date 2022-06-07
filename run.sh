36d151a0678ee0aee55e702e09b84fc03181fc5e
Mathias Bynens <mathias@qiwi.be>
2022-06-07 20:03:34 +0200

    Add Peloria

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 date="$(date +'%Y-%m-%d')";
-node --no-warnings get-fraggers.mjs 'We dont mind' > "data/vunira/fraggers-${date}.json";
 node --no-warnings get-fraggers.mjs 'We dont care' 'Zero Respect' > "data/antica/fraggers-${date}.json";
+node --no-warnings get-fraggers.mjs 'Reapers' > "data/peloria/fraggers-${date}.json";
 node --no-warnings get-fraggers.mjs 'No Impact' > "data/thyria/fraggers-${date}.json";
+node --no-warnings get-fraggers.mjs 'We dont mind' > "data/vunira/fraggers-${date}.json";
