15dadc5db86a1d257046b8c2caba8069d3583272
Mathias Bynens <mathias@qiwi.be>
2023-09-21 17:00:38 +0200

    Tweak world name

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 date="$(date +'%Y-%m-%d')";
-node --no-warnings get-fraggers.mjs 'Core' > "data/bastia/fraggers-${date}.json";
-node --no-warnings get-fraggers.mjs 'Nevermind' > "data/vunira/fraggers-${date}.json";
+node --no-warnings get-fraggers.mjs 'Core' > "data/retalia/fraggers-${date}.json";
