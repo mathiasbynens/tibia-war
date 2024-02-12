3382df328585dba636031268d4f80f97e4d05c63
Mathias Bynens <mathias@qiwi.be>
2024-02-13 00:25:03 +0100

    Add Peloria

--- run.sh
+++ run.sh
@@ -2,3 +2,4 @@
 
 date="$(date +'%Y-%m-%d')";
 node --no-warnings get-fraggers.mjs 'Core' > "data/retalia/fraggers-${date}.json";
+node --no-warnings get-fraggers.mjs 'Sleepers' > "data/peloria/fraggers-${date}.json";
